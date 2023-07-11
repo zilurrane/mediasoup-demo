4fbc5c6fb6607a5ce5e6fa057f5f2167b79667ab
zilurrane <zilurrane@gmail.com>
2023-07-11 18:28:35 +0530

    Added ConferenceWindow Header and Footer

--- web/src/components/layout/conference-window.jsx
+++ web/src/components/layout/conference-window.jsx
@@ -1,4 +1,8 @@
 const ConferenceWindow = () => {
-    return <div>Testing</div>
+    return <div className="flex flex-col h-screen justify-between">
+        <div className="h-5 bg-white dark:bg-black">Header</div>
+        <div>Body</div>
+        <div className="h-5 bg-white dark:bg-black">Footer</div>
+    </div>
 }
 export default ConferenceWindow;
